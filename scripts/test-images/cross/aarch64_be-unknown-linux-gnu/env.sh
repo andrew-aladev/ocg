78f17bc38df7a96a8b07a097042fa323457c0eaa
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-01-25 16:07:32 +0300

    test images cosmetics

--- scripts/test-images/cross/aarch64_be-unknown-linux-gnu/env.sh
+++ scripts/test-images/cross/aarch64_be-unknown-linux-gnu/env.sh
@@ -4,5 +4,5 @@ set -e
 DIR=$(dirname "${BASH_SOURCE[0]}")
 source "${DIR}/../../env.sh"
 
-FROM_IMAGE_NAME="aarch64_be-unknown-linux-gnu"
+FROM_IMAGE_NAME="test_aarch64_be-unknown-linux-gnu"
 IMAGE_NAME="${IMAGE_PREFIX}_aarch64_be-unknown-linux-gnu"
