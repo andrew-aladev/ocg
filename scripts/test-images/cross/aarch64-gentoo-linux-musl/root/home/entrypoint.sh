151b354757cb1a8d64fb66da25eb5819fe91f9d8
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-08-20 23:34:27 +0300

    added aarch gentoo images

---
+++ scripts/test-images/cross/aarch64-gentoo-linux-musl/root/home/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+DIR=$(dirname "${BASH_SOURCE[0]}")
+cd "$DIR"
+
+env-update
+source "/etc/profile"
+
+git clone "https://github.com/andrew-aladev/ocg.git" \
+  --single-branch \
+  --branch "master" \
+  --depth 1 \
+  "ocg"
+
+cd "ocg"
+./scripts/ci_test.sh
