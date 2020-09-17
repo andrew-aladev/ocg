a849f15927975d6fc838ab2613c1420649dda3e5
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-09-17 23:32:46 +0300

    test image can mount current source itself without clone

--- scripts/ci_test.sh
+++ scripts/ci_test.sh
@@ -4,22 +4,19 @@ set -e
 DIR=$(dirname "${BASH_SOURCE[0]}")
 cd "$DIR"
 
-# This script is for CI machines only, it provides junk and changes some config files.
-# Please do not use it on your machine.
-
 cd ".."
 
-# CI may not want to provide target ruby version.
-# We can just use the latest available ruby based on target major version.
+ruby_version=$(< ".ruby-version")
+ruby_gemset=$(< ".ruby-gemset")
+
 if command -v rvm > /dev/null 2>&1; then
-  ruby_version=$(< ".ruby-version")
+  # Using latest available ruby version (search is based on required major version).
   ruby_major_version=$(echo "${ruby_version%.*}" | sed "s/\./\\\./g") # escaping for regex
   ruby_version=$(rvm list | grep -o -e "${ruby_major_version}\.[0-9]\+" | sort | tail -n 1)
-  echo "${ruby_version}" > ".ruby-version"
 fi
 
 bash -cl "\
-  rvm use '.'; \
+  rvm use \"${ruby_version}@${ruby_gemset}\"; \
   gem install bundler && \
   bundle install && \
   bundle exec rake \
