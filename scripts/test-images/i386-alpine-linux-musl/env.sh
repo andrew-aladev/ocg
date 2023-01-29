0328ca8c7fe1c2c48646c8d2d01c4ffdd8569714
Andrew Aladjev <aladjev.andrew@gmail.com>
2023-01-29 13:54:28 +0300

    updated test images scripts

--- scripts/test-images/i386-alpine-linux-musl/env.sh
+++ scripts/test-images/i386-alpine-linux-musl/env.sh
@@ -5,4 +5,4 @@ DIR=$(dirname "${BASH_SOURCE[0]}")
 source "${DIR}/../env.sh"
 
 TARGET="i386-alpine-linux-musl"
-IMAGE_NAME="${IMAGE_PREFIX}_${TARGET}"
+IMAGE_NAME="${DOCKER_HOST}/${DOCKER_USERNAME}/${IMAGE_PREFIX}_${TARGET}"
--- scripts/test-images/x86_64-pc-linux-gnu/env.sh
+++ scripts/test-images/x86_64-pc-linux-gnu/env.sh
@@ -5,4 +5,4 @@ DIR=$(dirname "${BASH_SOURCE[0]}")
 source "${DIR}/../env.sh"
 
 TARGET="x86_64-pc-linux-gnu"
-IMAGE_NAME="${IMAGE_PREFIX}_${TARGET}"
+IMAGE_NAME="${DOCKER_HOST}/${DOCKER_USERNAME}/${IMAGE_PREFIX}_${TARGET}"
